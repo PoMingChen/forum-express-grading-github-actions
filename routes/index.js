dbca3f72f695ae902b335d2800332e10ce85e1ed
陳柏銘 <alexchen000003@gmail.com>
2024-10-23 23:20:36 +0800

    feat: add admin index page

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,8 @@ const router = express.Router()
 const restController = require('../controllers/restaurant-controller')
 
 //新增
-
+const admin = require('./modules/admin') //新增這行，載入 admin.js
+router.use('/admin', admin) //新增這行
 router.get('/restaurants', restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
