6f3894f20624f0ea234adc14fc9889b148a9a7c0
陳柏銘 <alexchen000003@gmail.com>
2024-10-27 22:50:29 +0800

    feat: admin restaurant page

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -2,7 +2,8 @@ const express = require('express')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 
-router.get('/restaurants/create', adminController.createRestaurant) // 新增這行
+router.get('/restaurants/create', adminController.createRestaurant) //留意路由擺放的順序，應從嚴格到寬鬆(例如：/restaurants/create 應該放在 /restaurants/:id 前面，依此類推；/restaurants/:id 應該放在 /restaurants 前面）
+router.get('/restaurants/:id', adminController.getRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', adminController.postRestaurant)
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
