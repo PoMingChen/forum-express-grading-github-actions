bdc4a03ec7ce77f11a593edab230804a33e6e24b
陳柏銘 <alexchen000003@gmail.com>
2024-11-07 21:01:51 +0800

    feat: category update(self-built)

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -8,8 +8,26 @@ const categoryController = {
     })
       .then(categories => res.render('admin/categories', {
         categories,
+        isEdit: false, // Indicate that this is the create route
         route: 'categories'
       }))
+      .catch(err => next(err))
+  },
+
+  // Handler for displaying the update category form
+  getCategory: (req, res, next) => {
+    Promise.all([
+      Category.findByPk(req.params.id, { raw: true }),
+      Category.findAll({ raw: true })
+    ])
+      .then(([category, categories]) => {
+        if (!category) throw new Error("Category doesn't exist!")
+        res.render('admin/categories', {
+          category,
+          categories,
+          isEdit: true // Indicate that this is the edit route
+        })
+      })
       .catch(err => next(err))
   },
 
@@ -20,6 +38,23 @@ const categoryController = {
     return Category.create({ name })
       .then(() => {
         req.flash('success_messages', 'category was successfully created')
+        res.redirect('/admin/categories')
+      })
+      .catch(err => next(err))
+  },
+
+  putCategory: (req, res, next) => {
+    const { name } = req.body
+    if (!name) throw new Error('Category name is required!')
+
+    // Handle asynchronous tasks concurrently
+    return Category.findByPk(req.params.id)
+      .then(category => {
+        if (!category) throw new Error("Category didn't exist!")
+        return category.update({ name })
+      })
+      .then(() => {
+        req.flash('success_messages', 'category was successfully to update')
         res.redirect('/admin/categories')
       })
       .catch(err => next(err))
