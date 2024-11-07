5a0a44b1266575836aa7d20b04f9da1ed8151a01
陳柏銘 <alexchen000003@gmail.com>
2024-11-07 23:22:44 +0800

    feat: add restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,9 +1,23 @@
-const restaurantController = { // restaurantController 是一個物件 (object)。
+const { Restaurant, Category } = require('../models')
 
-  // restaurantController 有不同的方法(Method），例如 getRestaurants ，這個方法目前是負責「瀏覽餐廳頁面」，也就是去 render 一個叫做 restaurants 的樣板。
+const restaurantController = {
 
   getRestaurants: (req, res) => {
-    return res.render('restaurants')
+    return Restaurant.findAll({
+      include: Category, // 一併拿出關聯的 Category model
+      nest: true,
+      raw: true
+    }).then(restaurants => {
+      const data = restaurants.map(r => ({
+        ...r,
+        description: r.description.substring(0, 50)
+      }))
+
+      return res.render('restaurants', {
+        restaurants: data
+      })
+    })
   }
 }
-module.exports = restaurantController // 記得要在檔案的最後一行，使用 module.exports 匯出 restaurantController，之後才能在其他檔案裡使用。
+
+module.exports = restaurantController
