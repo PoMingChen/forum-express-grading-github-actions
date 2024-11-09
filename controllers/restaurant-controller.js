ded66bea1bf1cd57edeb3d8a9817a4fc144a2c89
陳柏銘 <alexchen000003@gmail.com>
2024-11-09 17:32:16 +0800

    feat: add categories navbar on restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -3,20 +3,30 @@ const { Restaurant, Category } = require('../models')
 const restaurantController = {
 
   getRestaurants: (req, res, next) => {
-    return Restaurant.findAll({
-      include: Category, // 一併拿出關聯的 Category model
-      nest: true,
-      raw: true
-    }).then(restaurants => {
-      const data = restaurants.map(r => ({
-        ...r,
-        description: r.description.substring(0, 50)
-      }))
+    const categoryId = Number(req.query.categoryId) || '' // 新增這裡，從網址上拿下來的參數是字串，先轉成 Number 再操作
 
-      return res.render('restaurants', {
-        restaurants: data
+    return Promise.all([
+      Restaurant.findAll({
+        include: Category,
+        where: { // 新增查詢條件
+          ...categoryId ? { categoryId } : {} // 檢查 categoryId 是否為空值（注意是先處理 trinity operator 的條件，然後 spread operator 才作用）
+        },
+        nest: true,
+        raw: true
+      }),
+      Category.findAll({ raw: true })
+    ])
+      .then(([restaurants, categories]) => {
+        const data = restaurants.map(r => ({
+          ...r,
+          description: r.description.substring(0, 50)
+        }))
+        return res.render('restaurants', {
+          restaurants: data,
+          categories,
+          categoryId // 新增這裡，把 categoryId 帶到前端(以供前端判斷是否要加上 active class)
+        })
       })
-    })
       .catch(err => next(err))
   },
 
