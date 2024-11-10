6e0a51c5ae418692ee9f84e9ed9c64d367ab3244
陳柏銘 <alexchen000003@gmail.com>
2024-11-10 15:23:54 +0800

    feat: show comments on restaurant page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,4 +1,4 @@
-const { Restaurant, Category } = require('../models')
+const { Restaurant, Category, Comment, User } = require('../models')
 const { getOffset, getPagination } = require('../helpers/pagination-helper')
 
 const restaurantController = {
@@ -41,9 +41,11 @@ const restaurantController = {
 
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-      include: Category, // 一併拿出關聯的 Category model
-      nest: true
-      // raw: true
+      // 修改以下
+      include: [
+        Category,
+        { model: Comment, include: User }
+      ]
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,7 +1,12 @@
 const dayjs = require('dayjs')
+const relativeTime = require('dayjs/plugin/relativeTime')
+
+// This line extends the core dayjs library with the relativeTime plugin. By calling dayjs.extend(relativeTime), you are adding the relative time functionality to the dayjs instance. This means that you can now use methods provided by the relativeTime plugin to format dates and times relatively.
+dayjs.extend(relativeTime)
 
 module.exports = {
   currentYear: () => dayjs().year(), //取得當年年份作為 currentYear 的屬性值，並導出
+  relativeTimeFromNow: a => dayjs(a).fromNow(), // 增加這裡，將日期轉換成相對時間
   ifCond: function (a, b, options) {
     return a === b ? options.fn(this) : options.inverse(this)
   },
