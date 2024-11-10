3476791c4909ac81518fb69c8df00db567e70770
陳柏銘 <alexchen000003@gmail.com>
2024-11-10 12:34:48 +0800

    feat: add post comment on restaurant page

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const passport = require('../config/passport') // 引入 Passport，需要他幫
 // 新增，載入 controller（object）
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
+const commentController = require('../controllers/comment-controller')
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 
@@ -22,6 +23,8 @@ router.get('/logout', userController.logout)
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
+
+router.post('/comments', authenticated, commentController.postComment)
 router.get('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
 
